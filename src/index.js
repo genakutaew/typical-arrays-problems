16a648d81772ffb3b7a31e2bbf2cf34f2d30582b
Kutaev Gennady Igorevich <gennady.kutaev@truetech.by>
2021-02-03 14:46:30 +0300

    feat: test passed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0
+  return array.reduce((acc,item)=>acc>item?item:acc, 10000);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length == 0) return 0
+    return array.reduce((acc,item)=>acc>item?acc:item, -10000);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length == 0) return 0
+    return (array.reduce((acc,item)=>acc+item, 0) / array.length).toFixed(2);
 }
